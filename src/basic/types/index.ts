96d05b726189429291c6fa3504041b6901b4bf3a
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-08-04 14:17:06 +0900

    feat : ProductWithUI, Notification, ProductForm, CouponForm, CartTotals  type 정의

---
+++ src/basic/types/index.ts
@@ -0,0 +1,32 @@
+import { CartItem, Coupon, Product } from "../../types";
+
+export interface ProductWithUI extends Product {
+  description?: string;
+  isRecommended?: boolean;
+}
+
+export interface Notification {
+  id: string;
+  message: string;
+  type: "error" | "success" | "warning";
+}
+
+export interface ProductForm {
+  name: string;
+  price: number;
+  stock: number;
+  description: string;
+  discounts: Array<{ quantity: number; rate: number }>;
+}
+
+export interface CouponForm {
+  name: string;
+  code: string;
+  discountType: "amount" | "percentage";
+  discountValue: number;
+}
+
+export interface CartTotals {
+  totalBeforeDiscount: number;
+  totalAfterDiscount: number;
+}
