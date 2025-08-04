f3b29e8e8fb26a0785903ca97f20af142b01ecab
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-08-04 14:48:48 +0900

    feat : AppProvider 추가

--- src/advanced/main.tsx
+++ src/advanced/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { AppProviders } from "./contexts/AppProviders";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </React.StrictMode>
+);
