f45ec42be492707f5f60c1219095f167eb09b15c
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-08-04 14:55:57 +0900

    refactor : product  관련 컴포넌트 분리

---
+++ src/advanced/components/card/ProductCartList.tsx
@@ -0,0 +1,33 @@
+import { ProductWithUI } from "../../types";
+import { ProductCartItem } from "./ProductCartItem";
+import { useCartContext } from "../../contexts/CartContext";
+import { useAppContext } from "../../contexts/AppContext";
+
+interface ProductCartListProps {
+  filteredProducts: ProductWithUI[];
+}
+
+export const ProductCartList = ({ filteredProducts }: ProductCartListProps) => {
+  const { getRemainingStock, addToCart } = useCartContext();
+  const { isAdmin } = useAppContext();
+
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {filteredProducts.map((product) => {
+          const remainingStock = getRemainingStock(product);
+
+          return (
+            <ProductCartItem
+              key={product.id}
+              product={product}
+              remainingStock={remainingStock}
+              onAddToCart={addToCart}
+              isAdmin={isAdmin}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
