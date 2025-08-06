451c7380b52d5069d740d0f5f00b75d0b1d07725
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-08-07 01:12:11 +0900

    Update project files and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/front_6th_chapter2-2/" : "/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
     },
-  })
-)
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests.js",
+  },
+});
